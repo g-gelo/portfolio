97bd1ce6d0ac754bd5539c2b4f01fe03f7fd95ce
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2024-06-01 07:53:11 +0800

    Kahit ano

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex border-2 border-black h-screen">
+    <!-- First Div -->
+    <div class="border-2 h-screen grow-[0.5] border-rose-500 middle">1</div>
+    <!-- Middle Div -->
+    <div class="border-2 h-screen grow-[2] border-indigo-500">
+      <div class="border-2 border-green-500 m-4 h-48">
+        <div class="flex justify-center">
+          <div class="m-2 grow h-20 border-2 border-black-500 middle">1</div>
+          <div class="m-2 grow h-20 border-2 border-gray-500 middle">2</div>
+          <div class="m-2 grow h-20 border-2 border-orange-500 middle">3</div>
+        </div>
+        <div class="flex">
+          <div class="mx-2 grow h-20 border-2 border-orange-500 middle">4</div>
+        </div>
+      </div>
+    </div>
+    <!-- Last Div -->
+    <div class="border-2 h-screen grow-[0.5] border-blue-500 middle">3</div>
+  </div>
+</template>
+
+<style>
+/* .parent {
+  height: 300px;
+  width: 300px;
+} */
+.middle {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Cart Number {{ route.params.id }}</h1>
+  <h1 v-if="route.params.id == 1">Show Cart number 1</h1>
+  <h1 v-else-if="route.params.id == 2">Show Cart number 2</h1>
+</template>
+
+<script setup>
+const route = useRoute();
+
+// When accessing /posts/1, route.params.id will be 1
+console.log(route.params.id);
+</script>
