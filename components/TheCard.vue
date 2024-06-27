435b0f86760d516bc56a113f416fa769858b6659
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2024-06-27 15:06:36 +0800

    Underconstruction of Button

--- components/TheCard.vue
+++ components/TheCard.vue
@@ -8,36 +8,61 @@
       <p class="text-center">{{ description }}</p>
     </div>
     <div>
-      <NuxtLink
-        :to="link"
+      <button
+        @click="showModal = true"
         class="btn btn-active bg-[#260b9caf] text-white"
-        target="_blank"
       >
         {{ buttonText }}
-      </NuxtLink>
+      </button>
+    </div>
+    <div
+      v-if="showModal"
+      class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+    >
+      <div class="bg-white rounded-lg p-6 space-y-4 w-1/2 middle flex-col">
+        <h2 class="text-2xl font-bold">Notice</h2>
+        <img src="/icons/wrench.svg" class="size-9" alt="Facebook" />
+        <p>This feature is under development.</p>
+        <button
+          @click="showModal = false"
+          class="btn bg-[#260b9caf] text-white"
+        >
+          Close
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 const props = defineProps({
   iconPath: {
     type: String,
+    required: true,
   },
   title: {
     type: String,
+    required: true,
   },
   description: {
     type: String,
+    required: true,
   },
   link: {
     type: String,
+    required: true,
   },
   buttonText: {
     type: String,
+    required: true,
   },
 });
+
+const showModal = ref(false);
 </script>
+
 <style scoped>
 .middle {
   display: flex;
